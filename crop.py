d61eed14b4813f66c22050cec176e97da0e9a1fb
Jeffrey Lipton <jeffreyilipton@gmail.com>
2014-11-09 23:43:57 -0500

    worked

--- crop.py
+++ crop.py
@@ -2,21 +2,23 @@ import cv2
 
 def cropImage(imgfilename,xs,ys):
 	img = cv2.imread(imgfilename)
-	crop_img = img[xs[0]:xs[1],ys[0]:ys[1]] 
+	crop_img = img[ys[0]:ys[1],xs[0]:xs[1]] 
 	return crop_img
 	
-if __name__ = "__main__":
+if __name__ == "__main__":
 	from os import listdir
 	from os.path import isfile,join
 	readpath = "./read"
-	files = [f for f in listdir(readpath) if isfile(join(readpath,f))]
-	returned = cropImage(files[2],[100,900],[100,900])
-	cv2.imshow("cropped",returned)
-	cv2.waitkey(0)
-	return
+	files = [f for f in listdir(readpath) if isfile(join(readpath,f)) and (".jpg" in f)]
+	print files
+	#returned = cropImage(join(readpath,files[0]),[108,2250],[84,3490])
+	#cv2.imshow("cropped",returned)
+	#cv2.waitKey(0)
+	
 	
 	writepath = "./write"
 	for file in files:
-		croped = cropImage(files[2],[100,900],[100,900])
-		cv2.imwrite(join(writepath,file)
+		croped = cropImage(join(readpath,file),[108,2200],[84,3490])
+		cv2.imwrite(join(writepath,file),croped)
+		print file
 	
